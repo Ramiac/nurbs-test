b04a5ee32144564e644cada1923e4021915e7ff0
Ramiac <rami.achcar@gmail.com>
2022-08-04 10:07:31 -0300

    chore: implementing Nurbs package and first tests in AppComponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Nurbs from "nurbs";
+import * as fromJSON from "../assets/19-regression.result.json";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nurbs-test';
+
+
+export class AppComponent implements OnInit {
+
+  json = fromJSON;
+
+
+
+
+  ngOnInit(): void {
+    console.log(this.json.linear);
+    this.testNurbs()
+
+
+  }
+
+  testNurbs(){
+
+    let points: number[][] = [];
+    // this.json.linear[0].vertices.forEach((v, i, a) => {
+    //   ?????
+    //   points.push(coord);
+    // })
+
+    console.log(points);
+
+
+    let curve = new Nurbs({
+        points: points,
+        degree: this.json.linear[0].curve_order,
+        knots: this.json.linear[0].knots,
+        boundary: 'closed',
+      });
+
+        console.log(curve.domain)
+
+        // console.log(curve.evaluate([], 2))
+
+  }
 }
